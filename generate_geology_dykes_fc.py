bb3be358b9e4249c9f8aae661456aa90f8f5dc28
Edmond Chuc <edmond@kurrawong.ai>
2023-08-11 14:58:24 +1000

    Add geology map scripts and data

---
+++ generate_geology_dykes_fc.py
@@ -0,0 +1,96 @@
+import time
+
+import httpx
+from rdflib import (
+    Graph,
+    Namespace,
+    RDF,
+    DCAT,
+    SDO,
+    Literal,
+    BNode,
+    DCTERMS,
+    XSD,
+    RDFS,
+    URIRef,
+)
+from shapely.geometry import box
+
+
+dataset_json_url = "https://ogcapi.bgs.ac.uk/collections/bgsgeology625kdykes?f=json"
+url = "https://ogcapi.bgs.ac.uk/collections/bgsgeology625kdykes/items?f=json&offset=0"
+
+BGS = Namespace("http://data.bgs.ac.uk/id/")
+GEOLOGY_MAP_DATASET = BGS.geology625k
+FEATURE_COLLECTION_IRI = URIRef("http://data.bgs.ac.uk/id/geology625k/dykes")
+GEO = Namespace("http://www.opengis.net/ont/geosparql#")
+
+
+def get_bbox(data):
+    crs = data["crs"]
+    bbox_coords = data["bbox"][0]
+    bbox_geometry = box(*bbox_coords)
+    wkt = bbox_geometry.wkt
+    return f"<{crs}> {wkt}"
+
+
+def generate():
+    graph = Graph()
+
+    response = httpx.get(dataset_json_url)
+    response.raise_for_status()
+
+    data = response.json()
+
+    iri = FEATURE_COLLECTION_IRI
+    graph.add((GEOLOGY_MAP_DATASET, RDFS.member, iri))
+    graph.add((iri, RDF.type, GEO.FeatureCollection))
+
+    name = Literal(data["title"])
+    graph.add((iri, SDO.name, name))
+
+    description = Literal(data["description"])
+    graph.add((iri, SDO.description, description))
+
+    keywords = [Literal(keyword) for keyword in data["keywords"]]
+    for keyword in keywords:
+        graph.add((iri, DCAT.keyword, keyword))
+
+    bbox = get_bbox(data["extent"]["spatial"])
+    geom = BNode()
+    graph.add((iri, GEO.hasBoundingBox, geom))
+    graph.add((geom, RDF.type, GEO.Geometry))
+    graph.add((geom, GEO.asWKT, Literal(bbox, datatype=GEO.wktLiteral)))
+
+    temporal_start = data["extent"]["temporal"]["interval"][0][0]
+    temporal_end = data["extent"]["temporal"]["interval"][0][1]
+    period_of_time = BNode()
+    if temporal_start is not None:
+        graph.add((iri, DCTERMS.temporal, period_of_time))
+        graph.add((period_of_time, RDF.type, DCTERMS.PeriodOfTime))
+        graph.add(
+            (period_of_time, DCAT.startDate, Literal(temporal_start, datatype=XSD.date))
+        )
+    if temporal_start is not None and temporal_end is not None:
+        graph.add(
+            (period_of_time, DCAT.endDate, Literal(temporal_end, datatype=XSD.date))
+        )
+
+    graph.serialize(
+        "spatial/geologymap/dykes-feature-collection.ttl", format="longturtle"
+    )
+
+
+def main() -> None:
+    starttime = time.time()
+
+    try:
+        generate()
+
+    finally:
+        endtime = time.time() - starttime
+        print(f"Completed in {endtime:0.2f} seconds")
+
+
+if __name__ == "__main__":
+    main()
---
+++ generate_geology_faults_features.py
@@ -0,0 +1,117 @@
+import time
+
+import httpx
+from rdflib import (
+    Graph,
+    Namespace,
+    RDF,
+    DCAT,
+    SDO,
+    Literal,
+    BNode,
+    DCTERMS,
+    XSD,
+    RDFS,
+    URIRef,
+)
+from shapely.geometry import MultiPolygon, Polygon, MultiLineString
+
+
+url = "https://ogcapi.bgs.ac.uk/collections/bgsgeology625kfaults/items?f=json&offset={}"
+
+BGS = Namespace("http://data.bgs.ac.uk/id/")
+GEOLOGY_MAP_DATASET = BGS.geology625k
+FEATURE_COLLECTION_IRI = URIRef("http://data.bgs.ac.uk/id/geology625k/faults")
+FEATURE_TYPE = "Faults"
+FEATURE_IRI = "http://data.bgs.ac.uk/id/geology625k/faults/{}"
+GEO = Namespace("http://www.opengis.net/ont/geosparql#")
+
+
+def get_wkt(data):
+    geom_type = data["type"]
+    coords = data["coordinates"]
+
+    if geom_type == "MultiPolygon":
+        geom = MultiPolygon([Polygon(coord[0]) for coord in coords])
+    elif geom_type == "MultiLineString":
+        geom = MultiLineString(coords)
+    else:
+        raise ValueError(f"Handling of geometry type {geom_type} not implemented.")
+
+    return geom.wkt
+
+
+def generate():
+    graph = Graph()
+
+    per_page = 100
+    page_num = 0
+
+    graph = Graph()
+
+    while True:
+        print(f"Processing page {page_num + 1}.")
+        response = httpx.get(url.format(page_num * per_page))
+        response.raise_for_status()
+
+        data = response.json()
+
+        features = data["features"]
+
+        if features:
+            for feature in features:
+                feature_id = feature["id"]
+                iri = URIRef(FEATURE_IRI.format(feature_id))
+                graph.add((iri, RDF.type, GEO.Feature))
+                graph.add(
+                    (iri, SDO.name, Literal(f"{FEATURE_TYPE} Feature {feature_id}"))
+                )
+                graph.add((FEATURE_COLLECTION_IRI, RDFS.member, iri))
+
+                try:
+                    wkt = get_wkt(feature["geometry"])
+                except Exception as err:
+                    raise RuntimeError(
+                        f"Error processing feature {feature_id} on page {page_num + 1}"
+                    ) from err
+
+                geom_bnode = BNode()
+                graph.add((iri, GEO.hasGeometry, geom_bnode))
+                graph.add((geom_bnode, RDF.type, GEO.Geometry))
+                graph.add(
+                    (geom_bnode, GEO.asWKT, Literal(wkt, datatype=GEO.wktLiteral))
+                )
+
+                properties = feature["properties"]
+
+                if properties:
+                    for key, value in properties.items():
+                        property_bnode = BNode()
+                        graph.add((iri, SDO.additionalProperty, property_bnode))
+                        graph.add((property_bnode, RDF.type, SDO.PropertyValue))
+                        graph.add((property_bnode, SDO.name, Literal(key)))
+                        graph.add((property_bnode, SDO.value, Literal(value)))
+
+            page_num += 1
+        else:
+            # We've reached the end of the feature collection.
+            break
+
+    graph.serialize(
+        "spatial/geologymap/faults-features.nt", format="ntriples", encoding="utf-8"
+    )
+
+
+def main() -> None:
+    starttime = time.time()
+
+    try:
+        generate()
+
+    finally:
+        endtime = time.time() - starttime
+        print(f"Completed in {endtime:0.2f} seconds")
+
+
+if __name__ == "__main__":
+    main()
