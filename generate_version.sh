f28671261b63bec2a689666e2814de1a224fba13
Edd Lewis - BGS <edlew@bgs.ac.uk>
2024-06-24 13:17:12 +0000

    init ci files

---
+++ generate_version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CURRENT_DATE=$(date +%Y.%m)
+VERSION="BGS Vocablaries $CURRENT_DATE"
+
+echo "Generated release version: $VERSION"
+echo $VERSION  # Output the version for use in other commands
